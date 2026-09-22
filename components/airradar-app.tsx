f053530c2cc8ffa577bdcfed13bf1817334fca3f
Martin Pomykal <martin@pomykal.cz>
2026-09-22 11:51:04 +0200

    chore: remove obsolete aircraft image import

--- components/airradar-app.tsx
+++ components/airradar-app.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import Link from "next/link";
-import Image from "next/image";
 import dynamic from "next/dynamic";
 import { useRouter, useSearchParams } from "next/navigation";
 import { useCallback, useEffect, useMemo, useRef, useState, type CSSProperties } from "react";
